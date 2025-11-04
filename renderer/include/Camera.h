01537f6813c20afbbc4f31e71335458631efb512
rune-suy <rune.suy@outlook.com>
2025-11-04 19:55:06 +0100

    camera

--- renderer/include/Camera.h
+++ renderer/include/Camera.h
@@ -5,10 +5,52 @@
 #ifndef AP_PACMAN_CAMERA_H
 #define AP_PACMAN_CAMERA_H
 
+#include <tuple>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include "world/Position.h"
 
 namespace renderer {
     class Camera {
+    public:
+        /**
+         * Projects the logical position to the screen position.
+         * @param posX Logical X position between -1 and 1;
+         * @param window The render window to project to.
+         * @return The screen X position in pixels.
+         */
+        static int projectX(float posX, const sf::RenderWindow& window);
 
+        /**
+         * Projects the logical position to the screen position.
+         * @param position Logical position;
+         * @param window The render window to project to.
+         * @return The screen X position in pixels.
+         */
+        static int projectX(const logic::Position& position, const sf::RenderWindow& window);
+
+        /**
+         * Projects the logical position to the screen position.
+         * @param posX Logical Y position between -1 and 1;
+         * @param window The render window to project to.
+         * @return The screen Y position in pixels.
+         */
+        static int projectY(float posY, const sf::RenderWindow& window);
+
+        /**
+         * Projects the logical position to the screen position.
+         * @param position Logical position;
+         * @param window The render window to project to.
+         * @return The screen Y position in pixels.
+         */
+        static int projectY(const logic::Position& position, const sf::RenderWindow& window);
+
+        /**
+         * Projects the logical position to the screen position.
+         * @param position Logical position;
+         * @param window The render window to project to.
+         * @return The screen position in pixels.
+         */
+        static std::tuple<int, int> project(const logic::Position& position, const sf::RenderWindow& window);
     };
 }
 
--- renderer/src/Camera.cpp
+++ renderer/src/Camera.cpp
@@ -3,3 +3,30 @@
 //
 
 #include "../include/Camera.h"
+
+int renderer::Camera::projectX(float posX, const sf::RenderWindow &window) {
+    posX += 1.0f; // shift from [-1, 1] to [0, 2]
+    posX /= 2.0f; // scale from [0, 2] to [0, 1]
+    posX *= static_cast<float>(window.getSize().x); // scale to window width
+    posX += (static_cast<float>(window.getSize().x)-posX) / 2.0f; // shift for zero centered x-axis
+    return static_cast<int>(posX);
+}
+
+int renderer::Camera::projectX(const logic::Position &position, const sf::RenderWindow &window) {
+    return projectX(position.getX(), window);
+}
+
+int renderer::Camera::projectY(float posY, const sf::RenderWindow &window) {
+    posY += 1.0f; // shift from [-1, 1] to [0, 2]
+    posY /= 2.0f; // scale from [0, 2] to [0, 1]
+    posY *= static_cast<float>(window.getSize().y); // scale to window height
+    return static_cast<int>(posY);
+}
+
+int renderer::Camera::projectY(const logic::Position &position, const sf::RenderWindow &window) {
+    return projectY(position.getY(), window);;
+}
+
+std::tuple<int, int> renderer::Camera::project(const logic::Position &position, const sf::RenderWindow &window) {
+    return {projectX(position.getX(), window), projectY(position.getY(), window)};
+}
