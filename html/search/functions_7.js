542f35d0879b20bb41d33e0e1043f5fb9d795631
runesuy <runesuy@users.noreply.github.com>
2025-11-20 18:39:44 +0000

    Auto-generated Doxygen documentation update a1aa0b37b7014915133e2037933ce322200edc2f

--- html/search/functions_7.js
+++ html/search/functions_7.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['handlecollision_0',['handleCollision',['../classlogic_1_1CollisionHandler.html#aac1be582476d665f443f656b3cc3ca93',1,'logic::CollisionHandler']]],
-  ['handlecollisions_1',['handleCollisions',['../classlogic_1_1CollisionHandler.html#adea96926e7f25db8c1f47e29046d244c',1,'logic::CollisionHandler']]],
+  ['handlecollision_0',['handleCollision',['../classlogic_1_1CollisionHandler.html#a954562b8dd5934077765a860829c7ca9',1,'logic::CollisionHandler']]],
+  ['handlecollisions_1',['handleCollisions',['../classlogic_1_1CollisionHandler.html#a52344f961077f9e44ebbb42576929dd2',1,'logic::CollisionHandler']]],
   ['handleworldevent_2',['handleWorldEvent',['../classlogic_1_1WorldObject.html#aa8afa55395cbe77bd51771c8e176aff9',1,'logic::WorldObject']]],
   ['hasobserver_3',['hasObserver',['../classlogic_1_1Observable.html#a54bfc543d21f11be7137efce418394c2',1,'logic::Observable']]],
   ['hasobservers_4',['hasObservers',['../classlogic_1_1Observable.html#ad7cb2d9214c5dd7f5aa8a36ef7ce43b9',1,'logic::Observable']]]
