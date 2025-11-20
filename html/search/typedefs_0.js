187c36014fc4dffb196481a6d93106b07b580158
runesuy <runesuy@users.noreply.github.com>
2025-11-20 18:43:24 +0000

    Auto-generated Doxygen documentation update 7845a25ee26b83a43623416ce9f28edfb451e3d1

--- html/search/typedefs_0.js
+++ html/search/typedefs_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['collisiontype_0',['CollisionType',['../classlogic_1_1SizedWorldObject.html#ab48c8a4be8cf40566e48fa99f90225e2',1,'logic::SizedWorldObject']]],
+  ['collisiontypet_0',['CollisionTypeT',['../classlogic_1_1SizedWorldObject.html#a059ee03db27e3962f0c11f71cf0e2bd0',1,'logic::SizedWorldObject']]],
   ['coordinatetype_1',['CoordinateType',['../classlogic_1_1ICoordinate.html#a3d35c6be08adc094e27f987c1be77ec1',1,'logic::ICoordinate']]]
 ];
