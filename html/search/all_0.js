560a14ce103859414d66b48a6b3116909af978f1
runesuy <runesuy@users.noreply.github.com>
2025-11-20 16:46:15 +0000

    Auto-generated Doxygen documentation update 0ac06ce1f26b6a5b00430de298826ddac8a765e8

--- html/search/all_0.js
+++ html/search/all_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['_5fispastoroncenter_0',['_isPastOrOnCenter',['../classlogic_1_1MovingEntityController.html#a73ec56f81d7b0e35cd2870596c47a904',1,'logic::MovingEntityController']]]
+  ['_5fispastcenter_0',['_isPastCenter',['../classlogic_1_1MovingEntityController.html#ac1e017f33e1035f879e14e4be09b455c',1,'logic::MovingEntityController']]],
+  ['_5fispastoroncenter_1',['_isPastOrOnCenter',['../classlogic_1_1MovingEntityController.html#a73ec56f81d7b0e35cd2870596c47a904',1,'logic::MovingEntityController']]]
 ];
