8d281e9191cd6a05cdfc8cfad899f4cedba68dc2
runesuy <runesuy@users.noreply.github.com>
2025-11-21 21:26:32 +0000

    Auto-generated Doxygen documentation update 0e4f34837e1eee853dab535aebfde74f0fe0d3e3

--- html/search/enumvalues_9.js
+++ html/search/enumvalues_9.js
@@ -5,5 +5,6 @@ var searchData=
   ['player_5fcoin_5fcollected_2',['PLAYER_COIN_COLLECTED',['../namespacelogic.html#aeb87c1b72e16073c553fd4533aeb4afba0abbfac680b278080a1352b6b4fd7947',1,'logic']]],
   ['player_5fghost_5fkilled_3',['PLAYER_GHOST_KILLED',['../namespacelogic.html#aeb87c1b72e16073c553fd4533aeb4afbaae299516b4540fd4409f6b6de7996e5a',1,'logic']]],
   ['player_5fkilled_4',['PLAYER_KILLED',['../namespacelogic.html#aeb87c1b72e16073c553fd4533aeb4afba13f184851fc4ff1736348aa246da2c7c',1,'logic']]],
-  ['power_5fup_5',['POWER_UP',['../classlogic_1_1TileMap.html#a5da8a4243a7f7605c4fa1d8c84db37f9a28d680f5271ac92872dc883921487129',1,'logic::TileMap']]]
+  ['player_5fkilled_5fw_5',['PLAYER_KILLED_W',['../namespacelogic.html#a38da8dce3c551ae0ba23d0180a863d98a0bd3ee9dba92cd00cb214d412d8d8d95',1,'logic']]],
+  ['power_5fup_6',['POWER_UP',['../classlogic_1_1TileMap.html#a5da8a4243a7f7605c4fa1d8c84db37f9a28d680f5271ac92872dc883921487129',1,'logic::TileMap']]]
 ];
