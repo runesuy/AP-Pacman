b5926af0d6630166b6a875c461013d1b55e47593
runesuy <runesuy@users.noreply.github.com>
2025-11-21 19:40:12 +0000

    Auto-generated Doxygen documentation update 33767dae16344721c12186122a2c3ca31674bbf5

--- html/search/variables_5.js
+++ html/search/variables_5.js
@@ -1,5 +1,7 @@
 var searchData=
 [
   ['size_0',['size',['../classrenderer_1_1CircleModule.html#a0078c75a8765bf667e19c05d94dc5978',1,'renderer::CircleModule::size'],['../classrenderer_1_1RectangleModule.html#a5b1a0910e64ea966a58c4b5f718e93d9',1,'renderer::RectangleModule::size']]],
-  ['spawn_5fchar_1',['SPAWN_CHAR',['../classrenderer_1_1TXTMapParser.html#a0bac9400d659be039d76e3726fd8b75b',1,'renderer::TXTMapParser']]]
+  ['spawn_5fchar_1',['SPAWN_CHAR',['../classrenderer_1_1TXTMapParser.html#a0bac9400d659be039d76e3726fd8b75b',1,'renderer::TXTMapParser']]],
+  ['spawnposition_2',['spawnPosition',['../classlogic_1_1EntityModel.html#a8d674700220bd1fed0f8c50e896bda8f',1,'logic::EntityModel']]],
+  ['spawnpositionset_3',['spawnPositionSet',['../classlogic_1_1EntityModel.html#a1736b426f60ad3c130bdbb7b381b6bc7',1,'logic::EntityModel']]]
 ];
