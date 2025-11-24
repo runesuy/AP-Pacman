1a77535e85dc27f796b5d20efc5423d846fd2eec
runesuy <runesuy@users.noreply.github.com>
2025-11-24 08:45:22 +0000

    Auto-generated Doxygen documentation update bb4592f69247005a1a554ff8554899ec70a3c6c5

--- html/search/functions_0.js
+++ html/search/functions_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['_5fispastcenter_0',['_isPastCenter',['../classlogic_1_1MovingEntityController.html#ac1e017f33e1035f879e14e4be09b455c',1,'logic::MovingEntityController']]],
-  ['_5fispastoroncenter_1',['_isPastOrOnCenter',['../classlogic_1_1MovingEntityController.html#a73ec56f81d7b0e35cd2870596c47a904',1,'logic::MovingEntityController']]]
+  ['_5fgettilescaledspeed_0',['_getTileScaledSpeed',['../classlogic_1_1MovingEntityController.html#a695645ec08000e5e158b719f3093c14e',1,'logic::MovingEntityController']]],
+  ['_5fispastcenter_1',['_isPastCenter',['../classlogic_1_1MovingEntityController.html#ac1e017f33e1035f879e14e4be09b455c',1,'logic::MovingEntityController']]],
+  ['_5fispastoroncenter_2',['_isPastOrOnCenter',['../classlogic_1_1MovingEntityController.html#a73ec56f81d7b0e35cd2870596c47a904',1,'logic::MovingEntityController']]]
 ];
